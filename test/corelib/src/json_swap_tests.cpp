35ffea30d8389b43b6c7b1c95e10c105eccae888
Daniel Parker <danielaparker@yahoo.com>
2023-05-10 18:39:54 -0400

    memory

--- test/corelib/src/json_swap_tests.cpp
+++ test/corelib/src/json_swap_tests.cpp
@@ -12,7 +12,7 @@
 
 using namespace jsoncons;
 
-void check_swap(const json& j1)
+void check_swap(const json& j1, const json& j2)
 {
     json j3 = j1;
     //json j4 = j2;
@@ -24,9 +24,9 @@ void check_swap(const json& j1)
 
 TEST_CASE("test_swap")
 {
-    //json j6 = "Small";
+    json j6 = "Small";
     json j7 = "String too large for small string";
 
-    check_swap(j7);
+    check_swap(j7,j6);
 }
 
